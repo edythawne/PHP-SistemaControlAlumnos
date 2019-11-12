5dc877452ccd35eef2ec516890f9fa3b1fdd61c6
edythawne <itsramper@gmail.com>
2019-11-12 13:25:17 -0600

    Initial Commit

--- app/Libraries/DomPdfLib.php
+++ app/Libraries/DomPdfLib.php
@@ -7,8 +7,11 @@ use Dompdf\Dompdf;
 
 class DomPdfLib extends Dompdf {
 
+    /**
+     * DomPdfLib constructor.
+     */
 	public function __construct() {
-		 parent::__construct();
+        parent::__construct();
 	}
 
 }
--- app/Views/templates/report/header.php
+++ app/Views/templates/report/header.php
@@ -6,35 +6,19 @@
 <body>
 <!-- Define header and footer blocks before your content -->
 <header>
-    <table>
-        <thead>
-            <tr align="center">
-                <th style="border: none">
-                    <img src="<?php echo getImageReport('gob.jpg');?>" width="200px" height="110px">
-                </th>
-                <th style="border: none">
-                    <img src="<?php echo getImageReport('sev.jpg');?>" width="200px" height="110px">
-                </th>
-                <th style="border: none">
-                    <img src="<?php echo getImageReport('ver.jpg');?>" width="180px" height="110px">
-                </th>
-                <th style="border: none">
+    <img src="<?php echo getImageReport('gob.jpg');?>" width="200px" height="110px">
+    <img src="<?php echo getImageReport('ver.jpg');?>" width="180px" height="110px">
+    <img src="<?php echo getImageReport('sev.jpg');?>" width="200px" height="110px">
+    <img src="<?php echo getImageReport('ford.jpeg');?>" width="110px" height="110px">
 
-                </th>
-                <th style="border: none">
-                    <img src="<?php echo getImageReport('ford.jpeg');?>" width="110px" height="110px">
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td colspan="5" style="border: none; text-align: left;">
-                DIRECCIÓN DE EDUCACIÓN PRIMARIA FEDERALIZADA <br>
-                ESCUELA PRIMARIA: FORD 32 <br>
-                CLAVE: 30DPR0676A <br>
-                ZONA ESCOLAR: 008 SECTOR EDUCATIVO: 07 <br>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+
+    <div>
+        <p>
+            DIRECCIÓN DE EDUCACIÓN PRIMARIA FEDERALIZADA <br>
+            ESCUELA PRIMARIA: FORD 32 <br>
+            CLAVE: 30DPR0676A <br>
+            ZONA ESCOLAR: 008 SECTOR EDUCATIVO: 07 <br>
+        </p>
+    </div>
+
 </header>
