b964a1af9c2dac5abfe00470a6d5c3a6791831d7
edythawne <itsramper@gmail.com>
2019-11-18 12:22:31 -0600

    Initial Commit

--- app/Views/templates/report/footer_sign_dir.php
+++ app/Views/templates/report/footer_sign_dir.php
@@ -1,8 +1,8 @@
 <footer>
     <div>
-        <b>Director de la Escuela</b> <br>
+        <b>Director de la Escuela</b>
         <div class="signature">
-            <b style="display: inline-block; border-top: 1px solid #000; padding-top:2px; width: 300px">
+            <b style="display: inline-block; border-top: 1px solid #000; padding-top:1px; width: 300px">
                 <?php
                     echo 'Prof. '.$html['director'][0]['nombre'].' '.$html['director'][0]['ape_paterno'].' '.$html['director'][0]['ape_materno'];
                 ?>
