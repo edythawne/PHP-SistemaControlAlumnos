5cbc571fa7efb11600da88abd5aacb612656bbf8
edythawne <itsramper@gmail.com>
2019-11-22 10:39:46 -0600

    Initial Commit

--- app/Views/templates/report/footer_sign_dir.php
+++ app/Views/templates/report/footer_sign_dir.php
@@ -2,7 +2,7 @@
     <div>
         <b>Director de la Escuela</b>
         <div class="signature">
-            <b style="display: inline-block; border-top: 1px solid #000; padding-top:1px; width: 300px">
+            <b style="display: inline-block; border-top: 1px solid #000; padding-top:1px; width: 290px">
                 <?php
                     echo 'Prof. '.$html['director'][0]['nombre'].' '.$html['director'][0]['ape_paterno'].' '.$html['director'][0]['ape_materno'];
                 ?>
---
+++ writable/logs/log-2019-11-22.php
@@ -0,0 +1,46 @@
+<?php defined('SYSTEMPATH') || exit('No direct script access allowed'); ?>
+
+CRITICAL - 2019-11-22 09:49:09 --> No such file or directory
+#0 /var/www/html/ford32/system/Database/BaseConnection.php(377): CodeIgniter\Database\MySQLi\Connection->connect(false)
+#1 /var/www/html/ford32/system/Database/BaseConnection.php(643): CodeIgniter\Database\BaseConnection->initialize()
+#2 /var/www/html/ford32/system/Database/BaseBuilder.php(1783): CodeIgniter\Database\BaseConnection->query('SELECT `Grupos`...', Array, false)
+#3 /var/www/html/ford32/app/Models/AlumnoModel.php(138): CodeIgniter\Database\BaseBuilder->get()
+#4 /var/www/html/ford32/app/Controllers/HomeController.php(22): App\Models\AlumnoModel->getInfGrados()
+#5 /var/www/html/ford32/system/CodeIgniter.php(847): App\Controllers\HomeController->index()
+#6 /var/www/html/ford32/system/CodeIgniter.php(338): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\HomeController))
+#7 /var/www/html/ford32/system/CodeIgniter.php(246): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#8 /var/www/html/ford32/index.php(45): CodeIgniter\CodeIgniter->run()
+#9 {main}
+CRITICAL - 2019-11-22 09:49:09 --> Uncaught ErrorException: print_r(): Property access is not allowed yet in /var/www/html/ford32/app/Views/errors/html/error_exception.php:100
+Stack trace:
+#0 [internal function]: CodeIgniter\Debug\Exceptions->errorHandler(2, 'print_r(): Prop...', '/var/www/html/f...', 100, Array)
+#1 /var/www/html/ford32/app/Views/errors/html/error_exception.php(100): print_r(Object(App\Controllers\HomeController), true)
+#2 /var/www/html/ford32/system/Debug/Exceptions.php(298): include('/var/www/html/f...')
+#3 /var/www/html/ford32/system/Debug/Exceptions.php(171): CodeIgniter\Debug\Exceptions->render(Object(mysqli_sql_exception), 500)
+#4 [internal function]: CodeIgniter\Debug\Exceptions->exceptionHandler(Object(mysqli_sql_exception))
+#5 {main}
+  thrown
+#0 [internal function]: CodeIgniter\Debug\Exceptions->shutdownHandler()
+#1 {main}
+CRITICAL - 2019-11-22 09:49:12 --> No such file or directory
+#0 /var/www/html/ford32/system/Database/BaseConnection.php(377): CodeIgniter\Database\MySQLi\Connection->connect(false)
+#1 /var/www/html/ford32/system/Database/BaseConnection.php(643): CodeIgniter\Database\BaseConnection->initialize()
+#2 /var/www/html/ford32/system/Database/BaseBuilder.php(1783): CodeIgniter\Database\BaseConnection->query('SELECT `Grupos`...', Array, false)
+#3 /var/www/html/ford32/app/Models/AlumnoModel.php(138): CodeIgniter\Database\BaseBuilder->get()
+#4 /var/www/html/ford32/app/Controllers/HomeController.php(22): App\Models\AlumnoModel->getInfGrados()
+#5 /var/www/html/ford32/system/CodeIgniter.php(847): App\Controllers\HomeController->index()
+#6 /var/www/html/ford32/system/CodeIgniter.php(338): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\HomeController))
+#7 /var/www/html/ford32/system/CodeIgniter.php(246): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#8 /var/www/html/ford32/index.php(45): CodeIgniter\CodeIgniter->run()
+#9 {main}
+CRITICAL - 2019-11-22 09:49:12 --> Uncaught ErrorException: print_r(): Property access is not allowed yet in /var/www/html/ford32/app/Views/errors/html/error_exception.php:100
+Stack trace:
+#0 [internal function]: CodeIgniter\Debug\Exceptions->errorHandler(2, 'print_r(): Prop...', '/var/www/html/f...', 100, Array)
+#1 /var/www/html/ford32/app/Views/errors/html/error_exception.php(100): print_r(Object(App\Controllers\HomeController), true)
+#2 /var/www/html/ford32/system/Debug/Exceptions.php(298): include('/var/www/html/f...')
+#3 /var/www/html/ford32/system/Debug/Exceptions.php(171): CodeIgniter\Debug\Exceptions->render(Object(mysqli_sql_exception), 500)
+#4 [internal function]: CodeIgniter\Debug\Exceptions->exceptionHandler(Object(mysqli_sql_exception))
+#5 {main}
+  thrown
+#0 [internal function]: CodeIgniter\Debug\Exceptions->shutdownHandler()
+#1 {main}
